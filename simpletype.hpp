7d56b7a168dc3d9a8f94a1f29cda8e21d943177a
harry <machengxue707@gmail.com>
2018-04-27 15:29:32 +0800

    add the file simpeletype

---
+++ simpletype.hpp
@@ -0,0 +1,22 @@
+#include <eosiolib/eosio.hpp>
+#include <string>
+using namespace eosio;
+
+
+namespace simple{
+
+    struct user_ticket{
+
+        uint64_t colleteral_token ;
+        uint64_t  col_token_num;
+        uint64_t lend_token;
+        uint64_t  lend_token_num;
+        uint64_t  lend_limit;
+        uint64_t  lend_uint64_terest;
+        uint64_t  platform_commission;
+        uint64_t autoclose;
+        //std::string text;
+        name textname;
+        
+        };
+}
